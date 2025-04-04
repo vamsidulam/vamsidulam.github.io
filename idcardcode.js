6c42bb9755140102c7f8fa1cedd6e0d697df695e
vamsidulam <151850002+vamsidulam@users.noreply.github.com>
2025-04-04 21:10:05 +0530

    Add files via upload

---
+++ idcardcode.js
@@ -0,0 +1,90 @@
+let pic='';
+        let name_person='';
+        let regno_person='';
+        let father_person='';
+        let adhaar_person='';
+        let village_person='';
+        let phone_person='';
+        
+        let reg_num='';
+        
+         document.querySelector('.js-idcard-box').innerHTML=``;
+         let idinputbox=document.querySelector('.js-input-box');
+        let idsearchbtn=document.querySelector('.js-search-btn');
+        idinputbox.addEventListener('keydown',(event)=>{
+            if(event.key==='Enter'){
+                search();
+            }
+        });
+        idsearchbtn.addEventListener('click',search);
+        function search(){
+            let value=0;
+            
+            reg_num=idinputbox.value;
+            console.log(idinputbox);
+            console.log(reg_num);
+            obj.forEach( (details) => {
+                
+                if(details.regno===reg_num){
+                    value=1;
+                    pic=details.photo;
+                    name_person=details.name;
+                    regno_person=details.regno;
+                    father_person=details.father;
+                    adhaar_person=details.adhaar;
+                    village_person=details.village;
+                    phone_person=details.phone;
+                    generateidcard();
+                }
+                
+            });
+            if(value===0)
+            {
+                document.querySelector('.js-idcard-box').innerHTML='';
+                alert('no id card found');
+            }
+        }
+        function generateidcard(){
+            document.querySelector('.js-idcard-box').innerHTML=`
+            <div class="idcardbox">
+        <div class="head">
+            <div class="logobox">
+                <img class="logo" src="sasilogopic.jpg" alt="no logo found">
+            </div>
+            <div class="textbox"><p>Values we live....</p></div>
+        </div>
+        <div class="body">
+            <div class="idimagebox">
+                <img class="idimage" src=${pic} alt="no id image found">
+            </div>
+            <div class="iddetailsbox">
+                <p>${name_person}</p>
+                <p>${regno_person}</p>
+                <p>B.Tech C.S.E</p>
+            </div>
+            <div class="barcodebox">
+                <img class="barcodeimage" src="barcodeimage.jpg" alt="no barcode found">
+            </div>
+        </div>
+        <div class="tail">
+        <div class="iddatabox">
+            <div class="leftdatabox">
+                <p>Father Name: ${father_person}</p>
+                <p>Adhaar no: ${adhaar_person}</p>
+                <p>cell no: ${phone_person}</p>
+                <p>Validity: 2023 - 27</p>
+            </div>
+            <div class="rightdatabox">
+                <p>Blood Group: B +ve</p>
+                <p>Village: ${village_person}</p>
+            </div>
+        </div>
+        <div class="signaturebox">
+            <img class="signature" src="signature.jpg" alt="no signature image found">
+            
+        </div>
+        <div class="signbox"><p class="sign">signature</p></div>
+        </div>
+    </div>
+            `;
+        } 
---
+++ idcarddata.js
@@ -0,0 +1,101 @@
+const obj=[
+    {
+        photo: 'idcardpics/AA.jpg',
+        name:'D.Leela Vamsi Krishna',
+        regno:'23K61A0546',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'Darsiparru',
+        phone: 9949471552
+    },
+    {
+        photo: 'idcardpics/maheshbob-1.jpg',
+        name:'P.Madhav Kumar',
+        regno:'23K61A05E3',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'Eduru',
+        phone: 8074482243
+    },
+    {
+        photo: 'idcardpics/ramcharan.jpeg',
+        name:'R.VaraPrasad',
+        regno:'23K61A05F8',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'Jatlapalem',
+        phone: 7330651193
+    },
+    {
+        photo: 'idcardpics/prabhas.jpeg',
+        name:'M.Chandu',
+        regno:'23K61A0501',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'bridge kindha',
+        phone: 9949471552
+    },
+    {
+        photo: 'idcardpics/anime.jpeg',
+        name:'E.Mani Varma',
+        regno:'23K61A0548',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'Tirupathi',
+        phone: 6281411139
+    },
+    {
+        photo: 'idcardpics/ntr-2.jpg',
+        name:'E.Keethi Varun',
+        regno:'23K61A0547',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'Tanuku',
+        phone: 6303858090
+    },
+    {
+        photo: 'idcardpics/maheshbob-2.jpeg',
+        name:'G.Aditya',
+        regno:'23K61A0566',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'dikku malina uru',
+        phone: 9391356544
+    },
+    {
+        photo: 'idcardpics/maheshbob-2.jpeg',
+        name:'B.Yaswanth',
+        regno:'24K61A0502',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'Penugonda',
+        phone: 9652958129
+    },
+    {
+        photo: 'idcardpics/ntr.jpg',
+        name:'G.Tarak',
+        regno:'23K61A0554',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'Jatlpalem',
+        phone: 7989585835
+    },
+    {
+        photo: 'idcardpics/aa-1.jpg',
+        name:'A.Rohith',
+        regno:'23K61A0507',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'edhokiti le',
+        phone: 7680081182
+    },
+    {
+        photo: 'idcardpics/jhonysins.jpeg',
+        name:'B.Karthikeya',
+        regno:'24K61A0504',
+        father:'srinivas',
+        adhaar: 478645551211,
+        village:'unguturu',
+        phone: 9949471552
+    }
+];
